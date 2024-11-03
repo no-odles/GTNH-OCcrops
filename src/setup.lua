c42c7b02d257c385076bba7a1718ef48afa850f0
no-odles <hugo.stackhouse@protonmail.me>
2024-11-03 22:47:36 +1100

    TODO:not be dumb

--- src/setup.lua
+++ src/setup.lua
@@ -23,10 +23,10 @@ end
 if #args >= 2 then
     repo = args[2]
 else
-    repo = 'https://raw.githubusercontent.com/no-odles/GTNH-OCcrops/refs/heads/main/src/'
+    repo = 'https://raw.githubusercontent.com/no-odles/GTNH-OCcrops/'
 end
 
 -- INSTALL
 for i=1, #scripts do
-    shell.execute(string.format('wget -f %s%s/%s', repo, branch, scripts[i]))
+    shell.execute(string.format('wget -f %s%s/%s/src/', repo, branch, scripts[i]))
 end
