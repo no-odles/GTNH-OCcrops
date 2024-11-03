ed9987ecedebb76e50b682d6c5bd467269f766d1
no-odles <hugo.stackhouse@protonmail.me>
2024-11-03 22:27:05 +1100

    added setip

---
+++ src/setup.lua
@@ -0,0 +1,32 @@
+local shell = require('shell')
+local args = {...}
+local branch
+local repo
+local scripts = {
+    'actions.lua',
+    'config.lua',
+    'database.lua',
+    'geolyse.lua'
+    -- 'events.lua',
+    'nav.lua',
+    'propogate.lua'
+}
+
+-- BRANCH
+if #args >= 1 then
+    branch = args[1]
+else
+    branch = 'main'
+end
+
+-- REPO
+if #args >= 2 then
+    repo = args[2]
+else
+    repo = 'https://raw.githubusercontent.com/no-odles/GTNH-OCcrops/'
+end
+
+-- INSTALL
+for i=1, #scripts do
+    shell.execute(string.format('wget -f %s%s/%s', repo, branch, scripts[i]))
+end
